7b48a7ae83183dd5b8115f3747acc37e756e6238
Jack Harris <144192094+4ubiks@users.noreply.github.com>
2025-08-04 10:17:55 -0500

    Delete Trinity_GUI-main directory

--- Trinity_GUI-main/preferences.py
+++
@@ -1,8 +0,0 @@
-import json
-
-def load_preferences():
-    with open("preferences.json", "r") as file:
-        return json.load(file)
-
-preferences = load_preferences()
-rtsp_url = preferences.get("rtsp_url")
